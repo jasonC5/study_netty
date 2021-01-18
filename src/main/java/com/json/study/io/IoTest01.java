303174bfd6ef3c91a45d85e4cdc6aa620be165ef
JasonC5 <chenj@migrsoft.com>
2021-01-18 17:50:25 +0800

    init

---
+++ src/main/java/com/json/study/io/IoTest01.java
@@ -0,0 +1,4 @@
+package com.json.study.io;
+
+public class IoTest01 {
+}
---
+++ src/main/java/com/json/study/netty/MyNetty.java
@@ -0,0 +1,18 @@
+package com.json.study.netty;
+
+/**
+ * netty实现 客户端
+ * netty实现 服务端
+ *
+ *
+ * nc -l
+ * nc
+ *
+ */
+
+public class MyNetty {
+
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
